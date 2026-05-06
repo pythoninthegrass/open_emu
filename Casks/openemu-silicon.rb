8a62ec8ac23b36e75ca47f595601257240cf4c5e
nickybmon <nick.r.blackmon@gmail.com>
2026-05-06 13:04:42 -0400

    chore: release v1.0.8 — update appcast and Homebrew cask

--- Casks/openemu-silicon.rb
+++ Casks/openemu-silicon.rb
@@ -1,6 +1,6 @@
 cask "openemu-silicon" do
   version "1.0.8"
-  sha256 "f536ca92244743f9fdb924d66184131f067faa09b2b4b3cd60c81fad6c60a5d8"
+  sha256 "a58cd983f2529464e597b02139ce55c846121f07da2316e53d57b2081268296e"
 
   url "https://github.com/nickybmon/OpenEmu-Silicon/releases/download/v#{version}/OpenEmu-Silicon.dmg"
   name "OpenEmu Silicon"
